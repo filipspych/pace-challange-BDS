a5a0e08c25ddb0a7cbf2abca20d60ce92f2c9204
adambiel22 <51017764+adambiel22@users.noreply.github.com>
2022-03-11 11:54:43 +0100

    UI mockup

---
+++ UI.py
@@ -0,0 +1,31 @@
+# UI component
+# czyli PZ udostępnia PZ.getGrafPoNastepnejIteracji(int wierzcholekONajwikszeCyklowosci),
+# PS udostępnia PS.getWierzcholekONajwiekszejCyklowosci(G)
+# dodatkowo PZ.rozpocznij(G) który resetuje PZ i przekazuje PZ że teraz PZ będzie pracowało na grafie G
+import igraph as ig
+
+
+def InitializeProcedure(G):
+    # PZ.rozpocznij(G)
+    result = []
+    g = G
+    while(True):
+        print('--------------------')
+        print('i - next iteration')
+        print('p - show graph')
+        x = input()
+        match x:
+            case 'i':
+                print('next iteration')
+                # g = NextStep(result, g)
+            case 'p':
+                print('showing plot')
+                # ig.plot(g)
+
+
+def NextStep(result, G):
+    v = PS.getWierzcholekONajwiekszejCyklowosci(G)
+    result.append(v)
+    return PZ.getGrafPoNastepnejIteracji()
+
+InitializeProcedure(5)
