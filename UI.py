f36aa375110e9a81058847a64641e4c7b4a63496
adambiel22 <51017764+adambiel22@users.noreply.github.com>
2022-03-23 15:35:18 +0100

    comparing with OPT

--- UI.py
+++ UI.py
@@ -14,25 +14,37 @@ import opt
 def InitializeProcedure(G: Graph):
     PZ.rozpocznij(G)
     result: list[int] = []
-    g: Graph = G
-    OPT = opt.getMFVSDokladny(G.copy())
+    g: Graph = G.copy()
+    skip = False
+    OPT = opt.getMFVSDokladny(G)
     while(g.vcount() > 0):
-        print('-------------------------------')
-        print('i - next iteration')
-        print('p - show graph')
-        print('r - show current result')
-        x = input()
-        match x:
-            case 'i':
-                print('next iteration')
-                g = NextStep(result, g)
-            case 'p':
-                print('showing plot')
-                plot.plotGraph(g)
-            case 'r':
-                print('Current result:' + str(result))
+        if (not skip):
+            print('-------------------------------')
+            print('i - next iteration')
+            print('p - show graph')
+            print('r - show current result')
+            print('s - skip iterations')
+            x = input()
+            match x:
+                case 'i':
+                    print('next iteration')
+                    g = NextStep(result, g)
+                case 'p':
+                    print('showing plot')
+                    plot.plotGraph(g)
+                case 'r':
+                    print('Current result:' + str(result))
+                case 's':
+                    skip = True
+        else:
+            g = NextStep(result, g)
+            
     print('Size of found feedback vertex set: ' + str(len(result)))
+    # x = input('If you want to compare with optimal solution, press \'o\'...')
     print('Size of minimal feedback vertex set: ' + str(len(OPT)))
+    # if (x == 'o'):
+    #     OPT = opt.getMFVSDokladny(G)    
+    #     print('Size of minimal feedback vertex set: ' + str(len(OPT)))
 
 
 def NextStep(result, G):
@@ -40,6 +52,6 @@ def NextStep(result, G):
     result.append(v)
     return PZ.getGrafPoNastepnejIteracji(v)
 
-(g, OPT) = gen.GenerateRandomGraph(8, 2)
+(g, OPT) = gen.GenerateRandomGraph(30, 3)
 print(OPT)
 InitializeProcedure(g)
