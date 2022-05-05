07f136d14bb53502723bb7ce2208f2d49d7a9a50
adambiel22 <51017764+adambiel22@users.noreply.github.com>
2022-05-05 09:30:55 +0200

    Revert "Feature/run app"

--- generating.py
+++ generating.py
@@ -1,5 +1,3 @@
-from calendar import c
-from itertools import cycle
 import igraph as ig
 import random
 import plot
@@ -7,74 +5,43 @@ import plot
 # k - the size of optimum DFVS solution. k <= n/2
 # ed - edge density (m=ed*n)
 # return - random graph with secified parameters
-def GenerateRandomGraph(n: int, k: int, ed: float):
+def GenerateRandomGraph(n, k, ed=0):
     # stwórz losowe drzewo o n wierzchołkach
     graph = ig.Graph.Tree_Game(n, directed=True)
     # plot.plotGraph(graph)
     # wylosuj wierzchołki, które mają należeć to OPT
     OPT =  random.sample(range(n), k)
 
-    # stwórz listę wierzchołków nie należących do rozwiązania. Lista posortowana topologicznie.
-    notOPT = [i for i in graph.topological_sorting() if i not in OPT]
-    for i in range(len(notOPT)):
-        notOPT[i] = (i, notOPT[i])
+    topologicalOrder = [i for i in graph.topological_sorting() if i not in OPT]
 
     # wygeneruj k wierzchołkowo-niezależnych cykli. (nie liczą się pętelki) 
-    # Na każdy wierzchołek z OPT musi przypadać
+    # Czyli na każdy wybrany wierzchołek musi przypadać
     # dokładnie jeden cykl. 
-    generateNodeIndependentCycles(graph, OPT, notOPT)
-
-    generateOverlappingCycles(graph, OPT, notOPT, ed)
+    generateNodeIndependentCycles(graph, OPT, n)
     return (graph, OPT)
 
-def generateOverlappingCycles(graph, OPT, notOPT, ed):
-    while(graph.vcount() * ed < graph.ecount()):
-        # wylosuj podzbiór z OPT oraz podzbiór z topologicalOrder
-        fromOPT = random.sample(OPT, random.randint(1, len(OPT)))
-        notFromOPT = random.sample(notOPT, random.randint(1, len(OPT)))
-        addCycleToGraph(graph, fromOPT, notFromOPT)
-
-def addCycleToGraph(graph: ig.Graph, fromOPT: list[int], notFromOPT: list[tuple]):
-    # posortuj verticiesNotFromOPT względem porządku topologicznego
-    notFromOPT.sort(key = lambda indexedVertex: indexedVertex[0])
-    cycle = [el[1] for el in notFromOPT]
-
-    # dodaj na koniec jeden wierzchołek z OPT
-    cycle.append(fromOPT.pop())
-    
-    # losowo powsadzaj wierzchołki z fromOPT do notFromOPT
-    indicies = [i for i in range(len(cycle))]
-    for vertex in fromOPT:
-        index = random.choice(indicies)
-        cycle.insert(index, vertex)
-        indicies.append(len(indicies))
-
-    # Stwórz krawędzie pomiędzy kolejnymi wierzchołkami z verticiesNotFromOPT
-    
-    graph.add_edges([
-        (cycle[v], cycle[(v + 1) % len(cycle)])
-        for v in range(len(cycle))
-        if not graph.are_connected(cycle[v], cycle[(v + 1) % len(cycle)])])
-
-
-def generateNodeIndependentCycles(graph: ig.Graph, OPT, notOPT: list[tuple]):
-    # Ze zbióru wierzchołków, które nie są w OPT należy wybrać k rozłącznych niepustych podzbiorów. W tym celu:
-    # Wylosuj k liczb z zakresu od 0 do n-k-1.
-    separators = random.sample(range(1, 1 + graph.vcount()), len(OPT))
+def generateNodeIndependentCycles(graph: ig.Graph, OPT, n):
+    # Ze zbióru wierzchołków, które nie są w OPT należy wybrać k rozłącznych niepustych podzbiorów.
+    #   Wylosuj k liczb z zakresu od 0 do n-k-1.
+    separators = random.sample(range(1, 1 + n), len(OPT))
     separators.append(0)
     separators.sort()
     # print("separators" + str(separators))
-    # Stwórz permutację wiezchołków, które nie są w OPT.
-    permutation = notOPT.copy()
+    #   Stwórz permutację podciągów, które nie są w OPT.
+    permutation = [x for x in range(n) if x not in OPT]
     # print(permutation)
     random.shuffle(permutation)
     # print(permutation)
-    # Stwórz listę list zawierających cykle odpowiadające wierzchołkom z OPT
+    #   Stwórz listę list zawierających cykle odpowiadające wierzchołkom z OPT
     cycles = [permutation[separators[i-1]:separators[i]] for i in range(1, len(OPT) + 1)]
     #   Stwórz każdy z cykli w grafie
     i = 0
     for cycle in cycles:
-        addCycleToGraph(graph, [OPT[i]], cycle)
+        cycle.append(OPT[i])
+        graph.add_edges([
+            (cycle[v], cycle[(v + 1) % len(cycle)])
+            for v in range(len(cycle))
+            if not graph.are_connected(cycle[v], cycle[(v + 1) % len(cycle)])])
         i += 1
 
 
